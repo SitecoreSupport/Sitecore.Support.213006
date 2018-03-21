5ff2055fd3b4f0ae7ef0370a6be43839888d68b6
DK\YMU <ymu@sitecore.net>
2018-03-21 14:34:01 +0200

    Original source code and VS files

---
+++ src/Sitecore.Support.213006/Pipelines/GetPageEditorNotifications/GetWorkflowNotification.cs
@@ -0,0 +1,77 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceEditor;
+using Sitecore.ExperienceEditor.Utils;
+using Sitecore.Globalization;
+using Sitecore.SecurityModel;
+using Sitecore.Shell.Applications.ContentManager.Panels;
+using Sitecore.Shell.Framework.CommandBuilders;
+using Sitecore.Workflows;
+using System;
+using Sitecore.Pipelines.GetPageEditorNotifications;
+
+namespace Sitecore.Support.Pipelines.GetPageEditorNotifications
+{
+  public class GetWorkflowNotification : GetPageEditorNotificationsProcessor
+  {
+    // Methods
+    private static string GetDescription(IWorkflow workflow, WorkflowState state, Database database) =>
+      WorkflowUtility.GetWorkflowStateDescription(workflow, state, database, "The item is in the '{0}' workflow state in the '{1}' workflow.");
+
+    public override void Process(GetPageEditorNotificationsArgs arguments)
+    {
+      Assert.ArgumentNotNull(arguments, "arguments");
+      if (!WebUtility.IsEditAllVersionsTicked())
+      {
+        using (new SecurityDisabler())
+        {
+          Item contextItem = arguments.ContextItem;
+          Database database = contextItem.Database;
+          IWorkflowProvider workflowProvider = database.WorkflowProvider;
+          if (workflowProvider != null)
+          {
+            IWorkflow workflow = workflowProvider.GetWorkflow(contextItem);
+            if (workflow != null)
+            {
+              WorkflowState state = workflow.GetState(contextItem);
+              if (state != null)
+              {
+                using (new LanguageSwitcher(WebUtility.ClientLanguage))
+                {
+                  WorkflowCommand[] commandArray;
+                  string description = GetDescription(workflow, state, database);
+                  string icon = state.Icon;
+                  PageEditorNotification item = new PageEditorNotification(description, PageEditorNotificationType.Information)
+                  {
+                    Icon = icon
+                  };
+                  using (new SecurityEnabler())
+                  {
+                    commandArray = WorkflowFilterer.FilterVisibleCommands(workflow.GetCommands(contextItem), contextItem);
+                  }
+                  if (WorkflowPanel.CanShowCommands(contextItem, commandArray))
+                  {
+                    foreach (WorkflowCommand command in commandArray)
+                    {
+                      string displayName = command.DisplayName;
+                      string str4 = new WorkflowCommandBuilder(contextItem, workflow, command).ToString();
+                      if (Settings.WebEdit.AffectWorkflowForDatasourceItems)
+                      {
+                        str4 = str4.Replace("item:workflow(", "webedit:workflowwithdatasourceitems(");
+                      }
+                      PageEditorNotificationOption option = new PageEditorNotificationOption(displayName, str4);
+                      item.Options.Add(option);
+                    }
+                  }
+                  arguments.Notifications.Add(item);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
---
+++ src/Sitecore.Support.213006/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.213006")]
+[assembly: AssemblyProduct("Sitecore.Support.213006")]
+[assembly: ComVisible(false)]
